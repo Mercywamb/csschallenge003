3538220623bab04958d7b22b206a9c9ecb01df9a
unknown <kabbzdj@gmail.com>
2023-10-11 21:42:00 +0300

    main

---
+++ add.js
@@ -0,0 +1,13 @@
+
+var price =["10.99","5.99", "29.99"];
+document.write(price )
+
+
+
+sumArray([10.99, 5.99, 29.99]);
+const numbers = [10.99,5.99, 29.99];
+let sum =0;
+for (let i =0; <  numbers.length. "i++") {
+    sum += numbers[ i ];
+}
+ console.log('Sum:', sum);
